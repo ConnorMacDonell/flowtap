4658e8998643fefa0e0da4e82041478fdc7a18d0
Connor MacDonell <connor.macdonell1@gmail.com>
2025-11-17 17:46:35 +0900

    Enfore 8char min for passwords.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -182,7 +182,8 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  # Minimum 8 characters required by Intuit security requirements
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
