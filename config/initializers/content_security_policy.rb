a7eeb4767116a646da5061c801cf5b4316b3bf17
Connor MacDonell <connor.macdonell1@gmail.com>
2025-11-20 09:16:20 +0900

    Create CSP initializer.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,22 +4,31 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+
+    # Allow scripts from self - nonces will be used for any inline scripts
+    policy.script_src  :self, :https
+
+    # Allow styles from self and unsafe-inline for Tailwind utility classes
+    policy.style_src   :self, :https, :unsafe_inline
+
+    # Allow connections to Stripe and Intuit APIs
+    policy.connect_src :self, :https, "https://api.stripe.com", "https://*.intuit.com"
+
+    # Allow iframes from Stripe (for checkout/payment forms)
+    policy.frame_src   "https://js.stripe.com", "https://checkout.stripe.com"
+  end
+
+  # Generate session nonces for permitted importmap and inline scripts
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w(script-src)
+
+  # Start in report-only mode to monitor violations without breaking functionality
+  # Once confirmed working, set to false to enforce the policy
+  config.content_security_policy_report_only = false
+end
